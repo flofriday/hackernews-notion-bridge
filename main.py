43ee833b319546589bdbea0d0209ae1544248347
flofriday <flohacksfriday@gmail.com>
2021-09-01 18:57:07 +0200

    Add richtext support for comments

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
-from typing import List, Optional, Union
+from typing import Any, List, Optional, Union
 from datetime import datetime
 from notion_client import Client as NotionClient
+from bs4 import BeautifulSoup
+import bs4
 import json
 import html
 import re
@@ -14,6 +16,10 @@ NUM_STORIES = 30
 TIMEOUT = 60  # The timeout is so high because we do many requests in paraleell
 
 
+class DownloadExcpetion(Exception):
+    pass
+
+
 @dataclass
 class Comment:
     """A Hackernews Comment"""
@@ -21,7 +27,7 @@ class Comment:
     by: str
     id: int
     comments: List["Comment"]
-    text: str
+    text: BeautifulSoup
     time: datetime
 
 
@@ -60,22 +66,25 @@ def process_comment_html(text: str) -> str:
     return text
 
 
-async def download_comment(
-    id: int, client: httpx.AsyncClient
-) -> Optional[Comment]:
+async def download_comment(id: int, client: httpx.AsyncClient) -> Comment:
     """Download a HN comment and all of its comments recurisvly"""
     # Download the comment
-    r = await client.get(
-        f"https://hacker-news.firebaseio.com/v0/item/{id}.json",
-        timeout=TIMEOUT,
-    )
+    try:
+        r = await client.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{id}.json",
+            timeout=TIMEOUT,
+        )
+    except httpx.TimeoutException as e:
+        raise DownloadExcpetion(f"Timeout downloading comment {id}") from e
+
     if r.status_code != 200:
-        logging.warning(f"Hackernews API returned {r.status_code}")
-        return None
+        raise DownloadExcpetion(
+            f"Hackernews API returned status: {r.status_code}"
+        )
 
     data = r.json()
     if data is None or "deleted" in data and data["deleted"]:
-        return None
+        raise DownloadExcpetion(f"Comment {id} is deleted")
 
     # First download the child-comments recursively:
     if "kids" in data:
@@ -92,7 +101,7 @@ async def download_comment(
         by=data["by"],
         id=data["id"],
         comments=comments,
-        text=process_comment_html(data["text"]),
+        text=BeautifulSoup(data["text"], "html.parser"),
         time=datetime.fromtimestamp(data["time"]),
     )
 
@@ -100,31 +109,42 @@ async def download_comment(
 async def download_story(id: int, client: httpx.AsyncClient) -> Story:
     """Download a HN story and all of its comments"""
     # Download the story
-    r = await client.get(
-        f"https://hacker-news.firebaseio.com/v0/item/{id}.json",
-        timeout=TIMEOUT,
-    )
+    try:
+        r = await client.get(
+            f"https://hacker-news.firebaseio.com/v0/item/{id}.json",
+            timeout=TIMEOUT,
+        )
+    except httpx.TimeoutException as e:
+        raise DownloadExcpetion(f"Timeout downloading story {id}") from e
+
     if r.status_code != 200:
-        logging.warning(f"Hackernews API returned {r.status_code}")
-        return None
+        raise DownloadExcpetion(
+            f"Hackernews API returned {r.status_code} downloading story {id}"
+        )
 
     data = r.json()
 
     # Ignore soties without an url for now
     # TODO: fix that in the future
     if "url" not in data:
-        logging.warning(f"Story {id} has no URL")
-        return None
+        raise DownloadExcpetion(f"Story {id} has no URL")
 
     # First download the child-comments recursively:
+    # TODO: continue here the switch to exception from optional types
+    comments = []
     if "kids" in data:
         comment_ids = data["kids"]
-        comments = await asyncio.gather(
-            *map(lambda c: download_comment(c, client), comment_ids)
+        comments_raw = await asyncio.gather(
+            *map(lambda c: download_comment(c, client), comment_ids),
+            return_exceptions=True,
         )
-        comments = [c for c in comments if c is not None]
-    else:
-        comments = []
+        for comment in comments_raw:
+            if isinstance(comment, DownloadExcpetion):
+                logging.warning(
+                    f"{str(comment)}\nSkipped comment {comment.args[0]}"
+                )
+                continue
+            comments.append(comment)
 
     # Now create the story
     return Story(
@@ -167,12 +187,13 @@ async def download_stories() -> List[Story]:
         while len(stories) < NUM_STORIES:
             story_id = top_stories[index]
             index += 1
-            story = await download_story(story_id, client)
-            if story is None:
-                logging.warning(f"Skipped story {story_id}")
+            logging.info(f"Downloading story {story_id}")
+            try:
+                story = await download_story(story_id, client)
+            except DownloadExcpetion as e:
+                logging.warning(f"{str(e)}\nSkipped story {story_id}")
                 continue
             stories.append(story)
-            logging.info(f"Downloaded story {story_id}")
 
     return stories
 
@@ -190,20 +211,74 @@ def properties_from_story(story: Story, position: int) -> dict:
     }
 
 
+def richtexts_from_html(tag, style=None) -> List[dict]:
+    if style is None:
+        style = dict()
+    else:
+        style = style.copy()
+
+    if isinstance(tag, bs4.element.NavigableString):
+        obj = {
+            "type": "text",
+            "text": {
+                "content": tag.string[: 2000 - 2],
+            },
+            "annotations": {},
+        }
+
+        if "bold" in style and style["bold"]:
+            obj["annotations"]["bold"] = True
+        if "italic" in style and style["italic"]:
+            obj["annotations"]["italic"] = True
+        if "code" in style and style["code"]:
+            obj["annotations"]["code"] = True
+        if "url" in style:
+            obj["text"]["link"] = dict()
+            obj["text"]["link"]["url"] = style["url"]
+        return [obj]
+
+    res = []
+    if tag.name == "i":
+        style["italic"] = True
+
+    if tag.name == "b":
+        style["bold"] = True
+
+    if tag.name == "a":
+        style["url"] = tag["href"]
+
+    for kid in tag.contents:
+        res += richtexts_from_html(kid, style)
+
+    if tag.name == "p":
+        kid[0]["text"]["content"] += "\n\n"
+
+    return res
+
+
 def block_from_comment(comment: Comment) -> dict:
     """Create Notion a block from a comment"""
+
+    try:
+        text = (richtexts_from_html(comment.text),)
+    except Exception as e:
+        logging.warning(f"Error parsing richtext comment {comment.id}: {e}")
+        text = [
+            {
+                "type": "text",
+                "text": {
+                    "content": comment.text.getText()[:2000],
+                },
+            }
+        ]
+
     block = {
         "object": "block",
         "type": "bulleted_list_item",
-        "bulleted_list_item": {
-            "text": [
-                {
-                    # Notion only allows up to 2000 characters
-                    "text": {"content": comment.text[:2000]},
-                },
-            ],
-        },
+        "bulleted_list_item": {"text": text},
     }
+
+    logging.info(json.dumps(richtexts_from_html(comment.text)))
 
     if len(comment.comments) != 0:
         children = [block_from_comment(c) for c in comment.comments]
@@ -229,10 +304,33 @@ def blocks_from_story(story: Story) -> list:
                 "text": [
                     {
                         "type": "text",
+                        "href": "/https://news.ycombinator.com/item?"
+                        f"id={story.id}",
                         "text": {
-                            "content": f"{story.score} Points · "
+                            "content": "on Hacker News",
+                            "link": {
+                                "url": "https://news.ycombinator.com/item?"
+                                f"id={story.id}",
+                            },
+                        },
+                    },
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": " · "
+                            f"{story.score} Points · "
                             f"{count_comments(story)} Comments · "
-                            f"by {story.by}",
+                            f"by ",
+                        },
+                    },
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": story.by,
+                            "link": {
+                                "url": "https://news.ycombinator.com/user?"
+                                f"id={story.by}",
+                            },
                         },
                     },
                 ],
@@ -274,12 +372,13 @@ def update_notion(stories: List[Story]):
     # Delete all old stories
     # TODO: instead of delelte, maybe just update their content
     kids = notion.databases.query(database_id)["results"]
+    logging.info(f"Deleting {len(kids)} old stories")
     for page in kids:
         notion.pages.update(page["id"], archived=True)
-    logging.info(f"Deleted {len(kids)} old stories")
 
     # Add the new stories
     for n, story in enumerate(stories):
+        logging.info(f"Uploading story {story.id}")
         properties = properties_from_story(story, n + 1)
         blocks = blocks_from_story(story)
         notion.pages.create(
@@ -287,7 +386,6 @@ def update_notion(stories: List[Story]):
             properties=properties,
             children=blocks,
         )
-        logging.info(f"Uploaded story {story.id}")
 
 
 async def main():
